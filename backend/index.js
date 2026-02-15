b0d7a62839d9968ba9ba1697fbe33e9ae7eb8dc1
svont <onlystemstuff@gmail.com>
2026-02-15 18:39:28 +0000

    Cors conflict solving

--- backend/index.js
+++ backend/index.js
@@ -25,11 +25,13 @@ const app=express();
 app.use(
   cors({
     origin: [
-      "http://localhost:3000", 
-      "http://127.0.0.1:5173",
-      "http://localhost:5173", 
-      "http://localhost:5174",
-      "http://127.0.0.1:5174"
+        "https://equihive-landing.vercel.app",
+        "https://equihive-dashboard.vercel.app",
+        "http://localhost:3000", 
+        "http://127.0.0.1:5173",
+        "http://localhost:5173", 
+        "http://localhost:5174",
+        "http://127.0.0.1:5174"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
